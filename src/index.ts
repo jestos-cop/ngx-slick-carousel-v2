dc9a87f962b76e4b85a07e0d48b1222bbb046869
Misha <gumanist225@gmail.com>
2025-04-15 15:23:37 +0300

    updated-to-angular-19

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {SlickCarouselComponent, SlickItemDirective} from './slick.component';
+
 export * from './slick.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    SlickCarouselComponent,
+    SlickItemDirective,
+  ],
+  exports: [
+    SlickCarouselComponent,
+    SlickItemDirective,
+  ]
+})
+export class SlickCarouselModule {
+}
